bd1eb2aecb79c739754597b6f437b42936588f00
Tetiana Melnyk <prostotayna@gmail.com>
2019-08-01 16:57:07 +0300

    homework-2

---
+++ src/models/costs.js
@@ -0,0 +1,36 @@
+const ObjectID = require('mongodb').ObjectID;
+
+const db = require('../db/db');
+
+exports.all = cb =>
+  db
+    .get()
+    .collection('costs')
+    .find()
+    .toArray((err, docs) => cb(err, docs));
+
+exports.findById = (id, cb) =>
+  db
+    .get()
+    .collection('costs')
+    .findOne({ _id: ObjectID(id) }, (err, doc) => cb(err, doc));
+
+exports.save = (cost, cb) =>
+  db
+    .get()
+    .collection('costs')
+    .insertOne(cost, (err, result) => cb(err, result));
+
+exports.update = (id, newData, cb) =>
+  db
+    .get()
+    .collection('costs')
+    .updateOne({ _id: ObjectID(id) }, { $set: newData }, (err, doc) =>
+      cb(err, doc),
+    );
+
+exports.delete = (id, cb) =>
+  db
+    .get()
+    .collection('costs')
+    .deleteOne({ _id: ObjectID(id) }, (err, result) => cb(err, result));
